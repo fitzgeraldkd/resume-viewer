4ca84497fa19018794946f3e3c8d39ec370ad113
Kenneth Fitzgerald <fitzgeraldkd@gmail.com>
2021-08-28 01:06:09 -0400

    Develop zoom feature

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 $(function () {
-    $("#control-zoomslider").on("input", updateZoom);
-    function updateZoom(e) {
-        console.log(e);
-        $(":root").css("--zoom-factor", e.target.value);
-    }
+    $("#control-zoomslider").on("input change", function (e) {
+        var input = e.target;
+        $(":root").css("--zoom", input.value);
+    });
+    $(".control-zoomin").on("click", function () {
+        var newZoom = parseInt($(":root").css("--zoom")) + 5;
+        newZoom = Math.min(newZoom, parseInt($("#control-zoomslider").attr("max")));
+        $("#control-zoomslider").val(String(newZoom));
+        $(":root").css("--zoom", newZoom);
+    });
+    $(".control-zoomout").on("click", function () {
+        var newZoom = parseInt($(":root").css("--zoom")) - 5;
+        newZoom = Math.max(newZoom, parseInt($("#control-zoomslider").attr("min")));
+        $("#control-zoomslider").val(String(newZoom));
+        $(":root").css("--zoom", newZoom);
+    });
 });
--- script.ts
+++ script.ts
@@ -1,8 +1,20 @@
 $(function() {
-    $("#control-zoomslider").on("input", updateZoom);
-
-    function updateZoom(e) {
-        console.log(e);
-        $(":root").css("--zoom-factor", e.target.value);
-    }
+    $("#control-zoomslider").on("input change", function(e) {
+        const input = e.target as HTMLInputElement;
+        $(":root").css("--zoom", input.value);
+    });
+    
+    $(".control-zoomin").on("click", function() {
+        let newZoom = parseInt($(":root").css("--zoom")) + 5;
+        newZoom = Math.min(newZoom, parseInt($("#control-zoomslider").attr("max")));
+        $("#control-zoomslider").val(String(newZoom));
+        $(":root").css("--zoom", newZoom);
+    });
+    
+    $(".control-zoomout").on("click", function() {
+        let newZoom = parseInt($(":root").css("--zoom")) - 5;
+        newZoom = Math.max(newZoom, parseInt($("#control-zoomslider").attr("min")));
+        $("#control-zoomslider").val(String(newZoom));
+        $(":root").css("--zoom", newZoom);
+    });
 });
