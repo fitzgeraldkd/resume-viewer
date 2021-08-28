7224b7875803eff1552e85d19d6fe0770d5512d8
Kenneth Fitzgerald <fitzgeraldkd@gmail.com>
2021-08-28 16:21:14 -0400

    Develop zoom-fit button

--- script.js
+++ script.js
@@ -6,13 +6,34 @@ $(function () {
     $(".control-zoomin").on("click", function () {
         var newZoom = parseInt($(":root").css("--zoom")) + 5;
         newZoom = Math.min(newZoom, parseInt($("#control-zoomslider").attr("max")));
-        $("#control-zoomslider").val(String(newZoom));
-        $(":root").css("--zoom", newZoom);
+        setZoom(newZoom);
+        // $("#control-zoomslider").val(String(newZoom));
+        // $(":root").css("--zoom", newZoom);
     });
     $(".control-zoomout").on("click", function () {
         var newZoom = parseInt($(":root").css("--zoom")) - 5;
         newZoom = Math.max(newZoom, parseInt($("#control-zoomslider").attr("min")));
-        $("#control-zoomslider").val(String(newZoom));
-        $(":root").css("--zoom", newZoom);
+        setZoom(newZoom);
+        // $("#control-zoomslider").val(String(newZoom));
+        // $(":root").css("--zoom", newZoom);
     });
+    $(".control-zoomfit").on("click", function () {
+        var windowWidth = $(window).width();
+        var windowHeight = $(window).height();
+        var documentWidth = 2 * styleProperty("--horizontal-margin") + 8.5 * styleProperty("--pixels-per-inch");
+        var documentHeight = 2 * styleProperty("--vertical-margin") + 11 * styleProperty("--pixels-per-inch");
+        var horizontalScale = windowWidth / documentWidth;
+        var verticalScale = windowHeight / documentHeight;
+        var newZoom = Math.min(horizontalScale, verticalScale) * 100;
+        console.log(windowWidth, windowHeight, documentWidth, documentHeight, horizontalScale, verticalScale);
+        setZoom(newZoom);
+    });
+    function setZoom(zoom) {
+        $("#control-zoomslider").val(String(zoom));
+        $(":root").css("--zoom", zoom);
+    }
+    function styleProperty(property) {
+        console.log(property, $(":root").css(property), parseInt($(":root").css(property)));
+        return parseInt($(":root").css(property));
+    }
 });
--- script.ts
+++ script.ts
@@ -5,16 +5,40 @@ $(function() {
     });
     
     $(".control-zoomin").on("click", function() {
-        let newZoom = parseInt($(":root").css("--zoom")) + 5;
+        let newZoom: number = parseInt($(":root").css("--zoom")) + 5;
         newZoom = Math.min(newZoom, parseInt($("#control-zoomslider").attr("max")));
-        $("#control-zoomslider").val(String(newZoom));
-        $(":root").css("--zoom", newZoom);
+        setZoom(newZoom);
+        // $("#control-zoomslider").val(String(newZoom));
+        // $(":root").css("--zoom", newZoom);
     });
     
     $(".control-zoomout").on("click", function() {
-        let newZoom = parseInt($(":root").css("--zoom")) - 5;
+        let newZoom: number = parseInt($(":root").css("--zoom")) - 5;
         newZoom = Math.max(newZoom, parseInt($("#control-zoomslider").attr("min")));
-        $("#control-zoomslider").val(String(newZoom));
-        $(":root").css("--zoom", newZoom);
+        setZoom(newZoom);
+        // $("#control-zoomslider").val(String(newZoom));
+        // $(":root").css("--zoom", newZoom);
     });
+
+    $(".control-zoomfit").on("click", function () {
+        const windowWidth: number = $(window).width();
+        const windowHeight: number = $(window).height();
+        const documentWidth: number = 2 * styleProperty("--horizontal-margin") + 8.5 * styleProperty("--pixels-per-inch");
+        const documentHeight: number = 2 * styleProperty("--vertical-margin") + 11 * styleProperty("--pixels-per-inch");
+        const horizontalScale: number = windowWidth / documentWidth;
+        const verticalScale: number = windowHeight / documentHeight;
+        const newZoom = Math.min(horizontalScale, verticalScale) * 100;
+        console.log(windowWidth, windowHeight, documentWidth, documentHeight, horizontalScale, verticalScale);
+        setZoom(newZoom);
+    });
+
+    function setZoom(zoom: number) {
+        $("#control-zoomslider").val(String(zoom));
+        $(":root").css("--zoom", zoom);
+    }
+
+    function styleProperty(property: string) {
+        console.log(property, $(":root").css(property), parseInt($(":root").css(property)))
+        return parseInt($(":root").css(property));
+    }
 });
